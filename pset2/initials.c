c44a1e86b89e16d673fb1c5054466ba75b7f3218
Ollegator1 <C:\Users\Oleg\AppData\Roaming\The Bat!>
2016-10-03 17:37:11 +0300

    add files

---
+++ pset2/initials.c
@@ -0,0 +1,57 @@
+/**
+ * CS50 Problem set 2
+ * Initials
+ * User: Oleg Konov
+ * Date: 20.09.2016
+ */
+
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)  
+{
+    void initialize(string name);
+
+    string name = GetString();
+
+    // Check if null
+    if (name != NULL)
+    {
+        initialize(name);
+    }
+
+    return 0;
+}
+
+void initialize(string name)  
+{
+    int start = 0;
+
+    // Get first letter as initial, checking for spaces
+    while (name[start] == ' ')
+    {
+        start++;
+    }
+
+    printf("%c", toupper(name[start]));
+
+    // Go through rest of name
+    for (int i = start + 1, n = strlen(name); i < n; i++)
+    {
+        // Get Letter after space
+        while (name[i] == ' ')
+        {
+            i++;
+
+            // Only print if next char is not a space
+            if (i < n && name[i] != ' ')
+            {
+                printf("%c", toupper(name[i]));
+            }
+        }   
+    }
+    // Print new line
+    printf("\n");
+}
